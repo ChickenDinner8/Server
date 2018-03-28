c6e79c409ad73234afe039ce6b50db2bc2f8013b
Koishi <1440089139@qq.com>
2018-03-28 14:24:16 +0800

    model prototype finished

--- ServerApp/models.py
+++ ServerApp/models.py
@@ -5,8 +5,12 @@ from django.db import models
 class Menu(models.Model):
     foodName = models.CharField(max_length=200)
     foodPrice = models.DecimalField(max_digits=4, decimal_places=1)
+    foodImage = models.ImageField()
 
 
 class Order(models.Model):
+    orderID = models.IntegerField(primary_key=True)
+    orderTime = models.TimeField()
+    tableNum = models.IntegerField()
+    foodOfOrder = models.CharField(max_length=1024)  # maybe JSON is a good choice?
     payment = models.DecimalField(max_digits=4, decimal_places=1)
-    # TODO: how to present food ordered by customer ?
