ad788844038cb3a9b0a63b722613480c1b81e7bf
Koishi <1440089139@qq.com>
2018-04-10 19:22:51 +0800

    add unit test

--- ChickenDinner8Server/settings.py
+++ ChickenDinner8Server/settings.py
@@ -29,7 +29,7 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
-    'ServerApp.apps.ServerConfig',
+    'ServerApp',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
