96b67528164d2bc237ff1de13761a632916ef805
RaymanNg <touhourayman@gmail.com>
2018-05-27 15:05:50 +0800

    it is a test for Travis

--- ServerApp/urls.py
+++ ServerApp/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('login', views.login, name='login'),
     path('restaurant', views.boss, name='bossHomepage'),
     path('customer', views.customer, name='customerHomepage'),
 ]
--- ServerApp/views.py
+++ ServerApp/views.py
@@ -2,6 +2,10 @@ from django.http import HttpResponse
 from django.shortcuts import render_to_response
 
 
+def login(request):
+    return HttpResponse("Please Login")
+
+
 def boss(request):
     return render_to_response("index.html")
 
