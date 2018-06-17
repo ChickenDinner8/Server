ae62a7d3f89083ef1b97594331fe71f268a60119
RaymanNg <touhourayman@gmail.com>
2018-06-17 11:05:02 +0800

    dockerize server

---
+++ ChickenDinner8/ServerApp/utils.py
@@ -0,0 +1,9 @@
+from django.http import HttpResponse
+import decimal
+import simplejson as json
+BOSS_USERNAME='username'
+BUYER_USERNAME='buyer_username'
+
+def eatDDJsonResponse(obj):
+    return HttpResponse(json.dumps(obj, use_decimal=True), content_type="application/json")
+
